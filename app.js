6e0afd20b432ed1d93dafd641b27f561f9145e3a
RoseTHERESA <rose.minta.theresa@gmail.com>
2015-09-10 09:10:33 -0700

    update for homework without bonuses

--- app.js
+++ app.js
@@ -6,28 +6,28 @@ var app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/add/:num1/:num2", function(request, response){
-	var result = request.params.num1 + request.params.num2;
-	response.render("math", {number:result});
+app.get("/add/:num1/:num2", function (req, res){
+	var result = Number(req.params.num1) + Number(req.params.num2);
+	res.render("math", {number:result});
 });
 
-app.get("/subract/:num1/:num2", function(request, response){
-	var result = request.params.num1 - request.params.num2;
-	response.render("math", {number:result});
+app.get("/subract/:num1/:num2", function (req, res){
+	var result = Number(req.params.num1) - Number(req.params.num2);
+	res.render("math", {number:result});
 });
 
-app.get("/multiply/:num1/:num2", function(request, response){
-	var result = request.params.num1 * request.params.num2;
-	response.render("math", {number:result});
+app.get("/multiply/:num1/:num2", function (req, res){
+	var result = Number(req.params.num1) * Number(req.params.num2);
+	res.render("math", {number:result});
 });
 
-app.get("/divide/:num1/:num2", function(request, response){
-	var result = request.params.num1 / request.params.num2;
-	response.render("math", {number:result});
+app.get("/divide/:num1/:num2", function (req, res){
+	var result = Number(req.params.num1) / Number(req.params.num2);
+	res.render("math", {number:result});
 });
 
 
 
 app.listen(3000, function(){
 	console.log("Server starting on port 3000");
-})
+});
